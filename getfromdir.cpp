e6b43c19b897ddb81badbb27ebc3455cf8d41efd
Pulkit Khandelwal <kpulkit95@gmail.com>
2016-07-22 15:44:40 +0530

    Add files via upload

---
+++ getfromdir.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+using namespace cv;
+using namespace std;
+int main()
+{
+
+    Mat frame;
+    VideoCapture capture;//("01.png");
+    capture.set(CV_CAP_PROP_POS_AVI_RATIO, 1);
+   while(1)
+    {
+       VideoCapture capture("%02d.png"); 
+             
+    capture >> frame;
+        capture.set(CV_CAP_PROP_POS_FRAMES, capture.get(CV_CAP_PROP_POS_FRAMES) - 2);
+ 
+        imshow("frame",frame);
+        if(waitKey(30)==27)
+            break;
+    }
+    return 0;
+} 
